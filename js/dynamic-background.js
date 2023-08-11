c3fd52bd3c64de7f386bfc6d00cd06824728c338
upasadena <underscore.pasadena@gmail.com>
2023-08-11 08:36:35 +0000

    Work on things

--- js/dynamic-background.js
+++ js/dynamic-background.js
@@ -48,144 +48,156 @@ function lastElement(arr) {
 
   // Start polling...
   checkReady(function($) {
-      // "Global" variables
-      // let previousBackground = undefined;
-      let lastScrollTop = 0;
-      let userScrolledDown = false;
-      const backgroundHistory = [];
+    // "Global" variables
+    // let previousBackground = undefined;
+    let lastScrollTop = 0;
+    let userScrolledDown = false;
+    const backgroundHistory = [];
+    const historyObj = {
+      objectAbove: undefined,
+      currentObject: undefined,
+      objectBelow: undefined,
+    };
 
-      // This function goes once jQuery is loaded
-      $(function() {
-        let endingTime = new Date().getTime();
-        let tookTime = endingTime - startingTime;
-        if (DEBUG) {
-          console.log("jQuery is loaded, after " + tookTime + " milliseconds!");
-        }
+    if (setDefaultBackground) {
+      historyObj.currentObject = defaultBackgroundId;
+    }
 
-        if (setDefaultBackground) {
-          $(".pb-12").addClass(defaultBackgroundId);
-          backgroundHistory.push(defaultBackgroundId);
-        }
-      });
+    if (DEBUG) console.log(historyObj);
 
-      // Check if the <div> is in the viewport
-      $.fn.isInViewport = function() {
-        /**
-         * The absolute (in the context of the page) y position of the
-         * elements, static
-         */
-        let elementTop = $(this).offset().top;
-        let elementBottom = elementTop + $(this).outerHeight();
-      
-        /**
-         * The top and bottom pixels that are seen
-         */
-        let viewportTop = $(window).scrollTop();
-        let viewportBottom = viewportTop + $(window).height();
+    // This function goes once jQuery is loaded
+    $(function() {
+      let endingTime = new Date().getTime();
+      let tookTime = endingTime - startingTime;
+      if (DEBUG) {
+        console.log("jQuery is loaded, after " + tookTime + " milliseconds!");
+      }
 
-        // Create center variables
-        const viewportCenter =
-          viewportTop + ((viewportBottom - viewportTop) / 2);
-        const viewportCenterTop = viewportCenter - centerAllowance;
-        const viewportCenterBottom = viewportCenter + centerAllowance;
-        // Check if it's within the center
-        const withinViewportCenter =
-          elementBottom > viewportCenterTop &&
-          elementTop < viewportCenterBottom;
+      if (setDefaultBackground) {
+        $(".pb-12").addClass(defaultBackgroundId);
+        backgroundHistory.push(defaultBackgroundId);
+      }
+    });
 
-        return withinViewportCenter;
-      };
-      
-      // Check if the user has scrolled up or down
-      $(window).scroll(function(event){
-        let st = $(this).scrollTop();
-        if (st > lastScrollTop){
-          // If the user scrolled down
-          userScrolledDown = true;
-          // console.log("downscroll");
-        } else {
-          // If the user scrolled up
-          // console.log("upscroll");
-          userScrolledDown = false;
-          // upscroll code
-        }
-        lastScrollTop = st;
-      });
-      
-      // On scroll
-      $(window).on("resize scroll", function() {
-        $(".bg").each(function() {
-          let activeBackground = $(this).attr("id");
-          
-          if ($(this).isInViewport()) {
-            // If the background hasn't changed, return
-            if (backgroundHistory && lastElement(backgroundHistory) == activeBackground) {
-              return;
-            }
+    // Check if the <div> is in the viewport
+    $.fn.isInViewport = function() {
+      /**
+       * The absolute (in the context of the page) y position of the
+       * elements, static
+       */
+      let elementTop = $(this).offset().top;
+      let elementBottom = elementTop + $(this).outerHeight();
+    
+      /**
+       * The top and bottom pixels that are seen
+       */
+      let viewportTop = $(window).scrollTop();
+      let viewportBottom = viewportTop + $(window).height();
 
-            if (DEBUG) console.log(`backgroundHistory: ${backgroundHistory}`);
+      // Create center variables
+      const viewportCenter =
+        viewportTop + ((viewportBottom - viewportTop) / 2);
+      const viewportCenterTop = viewportCenter - centerAllowance;
+      const viewportCenterBottom = viewportCenter + centerAllowance;
+      // Check if it's within the center
+      const withinViewportCenter =
+        elementBottom > viewportCenterTop &&
+        elementTop < viewportCenterBottom;
 
-            // If the user scrolled down or up
-            if (userScrolledDown) {
-              // if (DEBUG) console.log("Downscroll mid detected");
-              // Remove previous styling if it exists
-              if (backgroundHistory) {
-                // if (DEBUG) console.log("Downscroll previousBg mid detected");
-                $(".pb-12").removeClass(lastElement(backgroundHistory));
-              }
+      return withinViewportCenter;
+    };
+    
+    // Check if the user has scrolled up or down
+    $(window).scroll(function(event){
+      let st = $(this).scrollTop();
+      if (st > lastScrollTop){
+        // If the user scrolled down
+        userScrolledDown = true;
+      } else {
+        // If the user scrolled up
+        userScrolledDown = false;
+        // upscroll code
+      }
+      lastScrollTop = st;
+    });
+    
+    // On scroll
+    $(window).on("resize scroll", function() {
+      $(".bg").each(function() {
+        let activeBackground = $(this).attr("id");
+        
+        if ($(this).isInViewport()) {
+          // if (DEBUG) console.log(`backgroundHistory: ${backgroundHistory}`);
 
-              // if (DEBUG) {
-              //   console.log("Downscroll if mid detected");
-              //   console.log("[before swap]")
-              //   console.log(`activeBackground: ${activeBackground}`);
-              //   console.log(`backgroundHistory: ${lastElement(backgroundHistory)}`);
-              // }
-
-              // Add new styling
-              $(".pb-12").addClass(activeBackground);
-              backgroundHistory.push(activeBackground);
-
-              // if (DEBUG) {
-              //   console.log("Downscroll if mid detected");
-              //   console.log("[after swap]")
-              //   console.log(`activeBackground: ${activeBackground}`);
-              //   console.log(`backgroundHistory: ${lastElement(backgroundHistory)}`);
-              //   console.log("======================================")
-              // }
-            } else {
-              // If user scrolled up
-              if (backgroundHistory) {
-                // if (DEBUG) {
-                //   console.log("Upscroll if mid detected");
-                //   console.log("[before swap]")
-                //   console.log(`activeBackground: ${activeBackground}`);
-                //   console.log(`backgroundHistory: ${lastElement(backgroundHistory)}`);
-                // }
-
-                $(".pb-12").removeClass(activeBackground);
-                let newClass = backgroundHistory.pop();
-                if (!newClass) return;
-                $(".pb-12").addClass(newClass);
-                backgroundHistory.push(newClass);
-
-                // Swap the variables
-                // let temp = activeBackground;
-                // activeBackground = previousBackground;
-                // previousBackground = temp;
-
-                // activeBackground = previousBackground;
-
-                // if (DEBUG) {
-                //   console.log("Upscroll if mid detected");
-                //   console.log("[after swap]")
-                //   console.log(`activeBackground: ${activeBackground}`);
-                //   console.log(`backgroundHistory: ${lastElement(backgroundHistory)}`);
-                //   console.log("======================================")
-                // }
-              } 
-            }
+          // If the background hasn't changed, return
+          if (historyObj.currentObject === activeBackground) {
+            return;
           }
-        });
+
+          // If the user scrolled down or up
+          if (userScrolledDown) {
+            // If user has scrolled down
+            historyObj.objectAbove = historyObj.currentObject;
+            historyObj.currentObject = activeBackground;
+
+            if (DEBUG) console.log(historyObj);
+
+            if (historyObj.objectAbove) {
+              $(".pb-12").removeClass(historyObj.objectAbove);
+            }
+
+            $(".pb-12").addClass(historyObj.currentObject);
+
+            // Remove previous styling if it exists
+            // if (backgroundHistory.length > 0) {
+            //   $(".pb-12").removeClass(lastElement(backgroundHistory));
+            // }
+
+            // // Add new styling
+            // $(".pb-12").addClass(activeBackground);
+            // backgroundHistory.push(activeBackground);
+          } else {
+            // If user scrolled up
+            historyObj.objectBelow = historyObj.currentObject;
+            historyObj.currentObject = activeBackground;
+
+            if (DEBUG) console.log(historyObj);
+
+            $(".pb-12").removeClass(historyObj.currentObject);
+            $(".pb-12").removeClass(defaultBackgroundId);
+
+            if (historyObj.objectAbove) {
+              $(".pb-12").addClass(historyObj.objectAbove);
+            } else if (setDefaultBackground) {
+              $(".pb-12").addClass(defaultBackgroundId);
+            }
+
+            // if (backgroundHistory.length > 1) {
+            //   $(".pb-12").removeClass(activeBackground);
+
+            //   backgroundHistory.pop();
+            //   $(".pb-12").addClass(lastElement(backgroundHistory));
+
+            //   // backgroundHistory.push(newClass);
+            // } else if (backgroundHistory.length > 0) {
+            //   // If there is only one item left
+
+            //   $(".pb-12").removeClass(activeBackground);
+
+            //   backgroundHistory.pop();
+            //   // $(".pb-12").addClass(backgroundHistory[1]);
+
+            //   if (setDefaultBackground) {
+            //     $(".pb-12").addClass(defaultBackgroundId);
+            //   }
+            // } else {
+            //   console.log(`else block`);
+            // }
+          }
+        } else {
+          if (DEBUG) console.log("NOT IN VIEWPORT");
+        }
       });
+    });
   });
 })();
