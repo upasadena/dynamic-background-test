8bdf1dedbbf17603e66c8fddb7dc59bc1e6b46f0
upasadena <underscore.pasadena@gmail.com>
2023-08-11 03:24:16 +0000

    Embed jQuery import into the file

--- js/dynamic-background.js
+++ js/dynamic-background.js
@@ -1,6 +1,6 @@
-// var red = $('#red-bg').offset().top - 100;
-// var blue = $('#blue-bg').offset().top - 100;
-// var purple = $('#purple-bg').offset().top - 100;
+// var red = $("#red-bg").offset().top - 100;
+// var blue = $("#blue-bg").offset().top - 100;
+// var purple = $("#purple-bg").offset().top - 100;
 
 // // $(function() {
 // //   $(window).scroll(function () {
@@ -15,41 +15,64 @@
 
 // $(document).scroll(function(){
 //   if($(this).scrollTop() > red) {   
-//       $('header').css({"background-color":"red"});
+//       $("header").css({"background-color":"red"});
 //   } else if($(this).scrollTop() > blue) {   
-//       $('header').css({"background-color":"blue"});
+//       $("header").css({"background-color":"blue"});
 //     } else if($(this).scrollTop() > purple) {   
-//         $('header').css({"background-color":"purple"});
+//         $("header").css({"background-color":"purple"});
 //   } 
 //   // else {
-//   //     $('header').css({"background-color":"#520833"});
+//   //     $("header").css({"background-color":"#520833"});
 //   // }
 // });
 
-$.fn.isInViewport = function() {
-  var elementTop = $(this).offset().top;
-  var elementBottom = elementTop + $(this).outerHeight();
+var startingTime = new Date().getTime();
+var script = document.createElement("script");
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"; // Check https://jquery.com/ for the current version
+document.getElementsByTagName("head")[0].appendChild(script);
 
-  var viewportTop = $(window).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
+// Poll for jQuery to come into existence
+var checkReady = function(callback) {
+  if (window.jQuery) {
+      callback(jQuery);
+  }
+  else {
+      window.setTimeout(function() { checkReady(callback); }, 20);
+  }
+};
+
+// Start polling...
+checkReady(function($) {
+  $(function() {
+      var endingTime = new Date().getTime();
+      var tookTime = endingTime - startingTime;
+      window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+  });
+});
+
+$.fn.isInViewport = function() {
+  let elementTop = $(this).offset().top;
+  let elementBottom = elementTop + $(this).outerHeight();
+
+  let viewportTop = $(window).scrollTop();
+  let viewportBottom = viewportTop + $(window).height();
 
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
+let previousColor = undefined;
 
-var previousColor = undefined;
-
-$(window).on('resize scroll', function() {
-  $('.color').each(function() {
-    var activeColor = $(this).attr('id');
+$(window).on("resize scroll", function() {
+  $(".color").each(function() {
+    let activeColor = $(this).attr("id");
     
     if ($(this).isInViewport()) {
       // console.log(`"${activeColor}" is in viewport!`);
       // console.log(`activeColor = ${activeColor}`);
       // console.log(`previousColor = ${previousColor}`);
-      // $('#fixed-' + activeColor).addClass(activeColor + '-active');
+      // $("#fixed-" + activeColor).addClass(activeColor + "-active");
 
-      if (previousColor != undefined) {
+      if (previousColor) {
         $(".pb-12").removeClass(previousColor);
       }
 
@@ -57,7 +80,7 @@ $(window).on('resize scroll', function() {
       previousColor = activeColor;
     }
     // } else {
-    //   // $('#fixed-' + activeColor).removeClass(activeColor + '-active');
+    //   // $("#fixed-" + activeColor).removeClass(activeColor + "-active");
     //   $(".pb-12").removeClass(activeColor);
     // }
   });
